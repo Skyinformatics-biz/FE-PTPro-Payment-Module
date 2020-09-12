aa3b370054ab20e775003914627879111061af6a
elias-cloudbiz <eitclouddev@gmail.com>
2020-09-12 20:20:28 +0200

    add initial subs module

---
+++ module/payment/confirmationpayment.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-confirmationpayment',
+  templateUrl: './confirmationpayment.component.html',
+  styleUrls: ['./confirmationpayment.component.css']
+})
+export class ConfirmationpaymentComponent implements OnInit {
+
+  ServiceCart = [];
+
+
+
+  constructor(private _location: Location, private renderer: Renderer2) {
+
+
+  }
+
+  ngOnInit() {
+
+    var temp = JSON.parse(localStorage.getItem('HealthcartService'));
+
+    try {
+      if (temp != null) {
+     
+        this.ServiceCart = temp;
+        this.clearItems();
+
+      }
+
+
+
+    } catch (error) {
+
+    }
+
+
+
+  }
+
+  backReturn() {
+    this._location.back();
+  }
+
+  clearItems() {
+    localStorage.removeItem("HealthcartService");
+  }
+
+
+
+
+}
